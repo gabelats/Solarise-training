7ebc234f425c1705f5f47b371b2bde8f1693c635
Chance Creger <chance.creger@gmail.com>
2024-02-09 13:39:55 -0700

    figured out how to connect bootstrap with react

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,30 +1,32 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function Header() {
   return (
-    <>
-      <Navbar bg="light">
-        <Container>
-          <Navbar.Brand href="#home" className="d-flex align-items-center">
-            <img
-              src='./assets/Full_logo.png'
-              alt="Logo"
-              width="30"
-              height="30"
-              className="mr-2"
-            />
-          </Navbar.Brand>
-          <Nav variant="pills bg-warning" defaultActiveKey="/home" className="me-auto">
-            <Nav.Link href="#home" className="text-dark">Home</Nav.Link>
-            <Nav.Link href="#script" className="text-dark">Script</Nav.Link>
-            <Nav.Link href="final-assessment" className="text-dark">Final Assessment</Nav.Link>
-            <Nav.Link href="#contact" className="text-dark">Contact</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+    <div>
+      <header>
+        <Navbar className="bg-body-warning">
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                alt=""
+                src="./src/assets/Small_logo.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{" "}
+              Solarize
+            </Navbar.Brand>
+            <Nav className="mr-2">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Scripts</Nav.Link>
+              <Nav.Link href="#pricing">Contact</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </header>
+    </div>
   );
 }
 
