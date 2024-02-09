7235e7c26cebdaad1f0a5bcf4724b8547f4dfc65
danielgoss22 <daniel.goss22@gmail.com>
2024-02-09 14:12:19 -0700

    created typeDefs, started resolver function for query, and updtated video schema

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const typeDefs = `
+  type Admin {
+    _id: ID
+    name: String
+    username: String
+    email: String
+    password: String
+    employees: [Employee]!
+  }
+
+  type Employee {
+    _id: ID
+    name: String
+    username: String
+    password: String
+    video: [Video]
+  }
+
+  type Video {
+    _id: ID
+    titel: String
+    video: String
+  
+  }
+
+  type Auth {
+    token: ID!
+    admin: Admin
+  }
+
+  type Query {
+    employees: [Employee]
+    employee(username: String!): Employee
+    videos: Video
+    video(videoId: ID!): Video
+    me: Admin
+  }
+
+  type Mutation {
+    addEmployee(name: String!, username: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addVideo(title: String!, videoLink: String!): Auth
+    
+    removeEmployee(employeeId: ID!): Auth
+    removeVideo(videoId: ID!): Auth
+  }
+`;
+
+module.exports = typeDefs;
