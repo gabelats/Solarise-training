50298148b0d91108122ab7c38f8f1a9ff4e75894
danielgoss22 <daniel.goss22@gmail.com>
2024-02-10 11:23:44 -0700

    added bcrypt functionality to admin and employee schemas

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -30,6 +30,15 @@ const adminSchema = new Schema({
   ],
 });
 
+adminSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
 adminSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
