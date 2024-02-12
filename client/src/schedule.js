51ec9f8de76caf8d66f3d1504c470d9b0968ad54
aalyman42 <aalyman42@gmail.com>
2024-02-12 11:18:41 -0700

    modules load dynamically

---
+++ client/src/schedule.js
@@ -0,0 +1,184 @@
+const schedule = [
+  {
+    day: 1,
+    title: "Day 1 Schedule",
+    rows: [
+      {
+        timeslot: "9:00-9:30",
+        lesson: "Onboarding, New hire docs, Sunbase, email, groupme set up",
+      },
+      {
+        timeslot: "9:30-12:00",
+        lesson: "Sunbase training videoWatch Solar 101",
+      },
+      {
+        timeslot: "12:00-12:30",
+        lesson: "Lunch",
+      },
+      {
+        timeslot: "12:30-2:00",
+        lesson: "D2D Intro video, D2D foundations Video, Chris Solar 101",
+      },
+      {
+        timeslot: "2:00-3:00",
+        lesson: "Q&A",
+      },
+      {
+        timeslot: "3:00-",
+        lesson: "Sunbase Canvassing and sales videos",
+      },
+      {
+        timeslot: "Homework",
+        lesson: "Solar Insure Video, Memorize pitch",
+      },
+    ],
+  },
+  {
+    day: 2,
+    title: "Day 2 Schedule",
+    rows: [
+      {
+        timeslot: "9:00-12:30",
+        lesson: "D2D winning at the door - Door approach with Pistol Pete",
+      },
+      {
+        timeslot: "12:30-1:00",
+        lesson: "Lunch",
+      },
+      {
+        timeslot: "1:00-1:30",
+        lesson: "Objections Videos",
+      },
+      {
+        timeslot: "1:30-2:45",
+        lesson: "Requesting Proposal,Practice Pitches",
+      },
+      {
+        timeslot: "2:45-",
+        lesson: "Knock",
+      },
+      {
+        timeslot: "Homework",
+        lesson: "Advanced Solar Foundations Video",
+      },
+    ],
+  },
+  {
+    day: 3,
+    title: "Day 3 Schedule",
+    rows: [
+      {
+        timeslot: "9:00-11:00",
+        lesson: "Live door pitch examples",
+      },
+      {
+        timeslot: "11:00-12:30",
+        lesson: "Chris Close Videos",
+      },
+      {
+        timeslot: "12:30-1:00",
+        lesson: "Lunch",
+      },
+      {
+        timeslot: "1:00-2:00",
+        lesson: "D2D Critic Solar",
+      },
+      {
+        timeslot: "2:00-2:45",
+        lesson: "Practice pitches",
+      },
+      {
+        timeslot: "2:45-",
+        lesson: "Knock",
+      },
+      {
+        timeslot: "Homework",
+        lesson: "Solar presenting principles videos",
+      },
+    ],
+  },
+  {
+    day: 4,
+    title: "Day 4 Schedule",
+    rows: [
+      {
+        timeslot: "9:00-12:30",
+        lesson: "D2D winning at the door - Door approach with Pistol Pete",
+      },
+      {
+        timeslot: "12:30-1:00",
+        lesson: "Lunch",
+      },
+      {
+        timeslot: "1:00-1:30",
+        lesson: "Objections Videos",
+      },
+      {
+        timeslot: "1:30-2:45",
+        lesson: "Requesting Proposal,Practice Pitches",
+      },
+      {
+        timeslot: "2:45-",
+        lesson: "Knock",
+      },
+      {
+        timeslot: "Homework",
+        lesson: "Advanced Solar Foundations Video",
+      },
+    ],
+  },
+  {
+    day: 5,
+    title: "Day 5 Schedule",
+    rows: [
+      {
+        timeslot: "9:00-10:30",
+        lesson: "Live in home- Sam Taggart",
+      },
+      {
+        timeslot: "10:30-11:00",
+        lesson: "Live presentation breakdown",
+      },
+      {
+        timeslot: "11:00-12:00",
+        lesson: "Selling Solar Jesse Plotnick",
+      },
+      {
+        timeslot: "12:00-12:30",
+        lesson: "Lunch",
+      },
+      {
+        timeslot: "12:30-2:00",
+        lesson: "Q&A, Practice pitches",
+      },
+      {
+        timeslot: "2:00-",
+        lesson: "Knock",
+      },
+    ],
+  },
+  {
+    day: "k",
+    title: "Homework/Resources",
+    rows: [
+      {
+        timeslot: "Day 1",
+        lesson: "Solar Insure Video, Memorize pitch",
+      },
+      {
+        timeslot: "Day 2",
+        lesson: "Advanced Solar Foundations Video",
+      },
+      {
+        timeslot: "Day 3",
+        lesson: "Solar presenting principles videos",
+      },
+      {
+        timeslot: "Day 4",
+        lesson: "Advanced sales tips D2D",
+      },
+    ],
+  },
+];
+
+export default schedule;
