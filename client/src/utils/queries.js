87a0561fcaab42bb8c5b122aa57e6c8b51ea38e1
aalyman42 <aalyman42@gmail.com>
2024-02-12 13:57:26 -0700

    added me query/ employee query.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -15,45 +15,16 @@ export const QUERY_EMPLOYEES = gql`
   }
 `;
 
-export const QUERY_THOUGHTS = gql`
-  query getThoughts {
-    thoughts {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-    }
-  }
-`;
-
-export const QUERY_SINGLE_THOUGHT = gql`
-  query getSingleThought($thoughtId: ID!) {
-    thought(thoughtId: $thoughtId) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        commentAuthor
-        createdAt
-      }
-    }
-  }
-`;
-
 export const QUERY_ME = gql`
   query me {
     me {
       _id
       username
       email
-      thoughts {
+      employees {
         _id
-        thoughtText
-        thoughtAuthor
-        createdAt
+        username
+        name
       }
     }
   }
