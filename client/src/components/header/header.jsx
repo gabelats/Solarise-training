1621f3ae080152361b226c4fbdd554142ffdf5f5
Chance Creger <chance.creger@gmail.com>
2024-02-14 12:57:20 -0700

    merge

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./header.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Auth from "../../utils/auth";
 
 function Header({ userLoggedIn, setUserLoggedIn }) {
