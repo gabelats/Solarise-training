24b5cf9758c9402aa410fa5743a2db8db732dc76
danielgoss22 <daniel.goss22@gmail.com>
2024-02-14 13:00:46 -0700

    fixed merge conflict

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -11,14 +11,11 @@ function Header({ userLoggedIn, setUserLoggedIn }) {
     Auth.logout();
   };
 
-<<<<<<< HEAD
-=======
   const userLogout = (event) => {
     event.preventDefault();
     setUserLoggedIn(false);
   };
 
->>>>>>> main
   return (
     <div>
       <header>
@@ -33,60 +30,6 @@ function Header({ userLoggedIn, setUserLoggedIn }) {
                 className="d-inline-block align-center img2"
               />{" "}
             </Navbar.Brand>
-<<<<<<< HEAD
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav
-                variant="pills"
-                defaultActiveKey="home"
-                className={
-                  Navbar.Collapse ? "flex-column flex-lg-row " : "flex-row"
-                }
-              >
-                <Link to="/" className="nav-link-custom m-2">
-                  Home
-                </Link>
-                <Link to="/Module/k" className="nav-link-custom m-2">
-                  Scripts
-                </Link>
-                <a
-                  href="https://d2du.lightspeedvt.com/"
-                  target="_blank"
-                  className="nav-link-custom m-2"
-                >
-                  Door 2 Door University
-                </a>
-              </Nav>
-              <Nav className="m-2">
-                {Auth.loggedIn() ? (
-                  <button onClick={logout} className="btn btn-custom">
-                    Admin Logout
-                  </button>
-                ) : (
-                  <Link
-                    eventkey={2}
-                    to="/Login"
-                    className="m-2 nav-link-custom"
-                  >
-                    Admin Login
-                  </Link>
-                )}
-                {userLoggedIn == true ? (
-                  <button onClick={logout} className="btn btn-custom">
-                    Logout
-                  </button>
-                ) : (
-                  <Link
-                    eventkey={2}
-                    to="/Login"
-                    className="nav-link-custom m-2 "
-                  >
-                    Employee Login
-                  </Link>
-                )}
-              </Nav>
-            </Navbar.Collapse>
-=======
             <Nav variant="pills" defaultActiveKey="home" className="mx-4">
               {Auth.loggedIn() == true ? (
                 <Link to="/Admin" className="nav-link-custom m-2">
@@ -123,7 +66,6 @@ function Header({ userLoggedIn, setUserLoggedIn }) {
                 Logout
               </button>
             </Nav>
->>>>>>> main
           </Container>
         </Navbar>
       </header>
